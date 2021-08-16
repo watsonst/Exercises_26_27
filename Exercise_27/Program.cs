b33e713eb3a7351f5fe89d506dbc1b08ae54d5d9
Stephanie <me@stephaniemariewatson.com>
2021-08-16 13:07:59 -0400

    Inital commit for exercises 26 and 27

---
+++ Exercise_27/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Exercise_27
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string answer = string.Empty;
+
+            do
+            {
+                Console.Write("Please enter some text: ");
+                string userInput = Console.ReadLine().ToLower();
+
+
+                char[] array = userInput.ToCharArray();
+                int numberOfConsonants = 0;
+
+
+                foreach (char ch in array)
+                {
+                    if (ch.ToString() != "a" || ch.ToString() != "e" || ch.ToString() != "i" || ch.ToString() != "o" || ch.ToString() != "u")
+                        numberOfConsonants++;
+                }
+
+                Console.WriteLine($"There were {numberOfConsonants} consonants.");
+
+                Console.Write("\nWould you like to continue? (y/n): ");
+                answer = Console.ReadLine().ToLower();
+
+                if (answer == "n")
+                {
+                    Console.WriteLine("Goodbye!");
+                    break;
+                }
+
+            } while (answer == "y");
+        }
+
+    }
+
+}
+
---
+++ Exercises_26_27/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Exercise_26
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string answer = string.Empty;
+
+            do
+            {
+                Console.Write("Please enter some text: ");
+                string userInput = Console.ReadLine().ToLower();
+
+
+                char[] array = userInput.ToCharArray();
+                int numberOfVowels = 0;
+
+
+                foreach (char ch in array)
+                {
+                    if (ch.ToString() == "a" || ch.ToString() == "e" || ch.ToString() == "i" || ch.ToString() == "o" || ch.ToString() == "u")
+                        numberOfVowels++;
+                }
+
+                Console.WriteLine($"There were {numberOfVowels} vowels.");
+
+                Console.Write("\nWould you like to continue? (y/n): ");
+                answer = Console.ReadLine().ToLower();
+
+                if (answer == "n")
+                {
+                    Console.WriteLine("Goodbye!");
+                    break;
+                }
+
+            } while (answer == "y");
+        }
+
+    }
+
+}
